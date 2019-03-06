2e8922c0743f4f948f72ad27a7ce1217855594ca
Eduard Jusufi <edwardjusufi@gmail.com>
2019-03-06 21:16:27 +0000

    Update Loading.js

--- components/Loading.js
+++ components/Loading.js
@@ -21,47 +21,7 @@ const styles = StyleSheet.create({
 })
 
 class Loading extends Component {
-  constructor() {
-    super()
-    this.anim = {
-      width: new Animated.Value(10),
-      translateX: new Animated.Value(-50)
-    }
-  }
-
-  componentDidMount() {
-    Animated.loop(Animated.parallel([
-      Animated.sequence([
-        Animated.timing(this.anim.width, {
-          toValue: 75,
-          easing: Easing.back(1),
-          duration: 750
-        }),
-        Animated.timing(this.anim.width, {
-          toValue: 10,
-          // easing: Easing.back(2),
-          duration: 250
-        }),
-        Animated.timing(this.anim.width, {
-          toValue: 75,
-          easing: Easing.back(1),
-          duration: 750
-        }),
-        Animated.timing(this.anim.width, {
-          toValue: 10,
-          // easing: Easing.back(2),
-          duration: 250
-        })
-      ]),
-      Animated.sequence([
-        Animated.timing(this.anim.translateX, { toValue: 50, easing: Easing.back(1), duration: 1000 }),
-        Animated.timing(this.anim.translateX, { toValue: -50, easing: Easing.back(1), duration: 1000 })
-      ])
-    ])).start()
-  }
-
   render() {
-    const { translateX, width } = this.anim
     if (this.props.loading) {
       return (
         <View style={styles.container}>
