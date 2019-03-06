eaf6401b2bf7d9ea950b34a87f217df8e9d05161
Eduard Jusufi <edwardjusufi@gmail.com>
2019-03-06 22:11:13 +0000

    Update TopBar.js

--- components/TopBar.js
+++ components/TopBar.js
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
   },
   row: {
     flexDirection: 'row',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     alignItems: 'center',
   },
   title: {
